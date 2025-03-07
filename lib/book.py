21799e9f24d408fd140c2cfa3bb27c3e173308a6
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-03-07 06:06:04 -0500

    Updated lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        self.current_page = 1
+
+    def get_title(self):
+        return self.title
+
+    def get_page_count(self):
+        return self.page_count
+
+    def turn_page(self, direction="forward"):
+        if direction == "forward":
+            self.current_page += 1
+            if self.current_page > self.page_count:
+                self.current_page = self.page_count
+                print("Flipping the page...wow, you read fast!")
+        elif direction == "backward":
+            self.current_page -= 1
+            if self.current_page < 1:
+                self.current_page = 1
+        else:
+            print("Invalid direction. Please use 'forward' or 'backward'.")
+
+    def current_page_number(self):
+        return self.current_page
+
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,64 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand: str, size: int):
+        if not isinstance(brand, str):
+            raise TypeError("Brand must be a string.")
+        if not isinstance(size, int) or size <= 0:
+            raise ValueError("Size must be an integer.")
+
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+        self.is_cobbled = False
+    def get_size(self):
+        return self.size
+
+    def cobble(self):
+
+        self.is_cobbled = True
+        self.condition = "New"
+        print("The shoe has been repaired.")
+
+    def is_cobbled_status(self):
+        return self.is_cobbled
+
+    def cobble_makes_new(self):
+        if self.is_cobbled:
+            new_shoe = Shoe(self.brand, self.size)
+            new_shoe.condition = "New"
+            print(f"A new cobbled version of the {self.brand} shoe (size {self.size}) has been created with condition {new_shoe.condition}.")
+            return new_shoe
+        else:
+            print(f"The {self.brand} shoe (size {self.size}) needs to be cobbled first.")
+            return None
+
+    def get_condition(self):
+        return self.condition
+
+
+
+
+
+
+
+
+
+
+
+
+    # def __init__(self, brand, size):
+    #     self.brand = brand
+    #     self._size = size
+
+    # def get_size(self):
+    #     return self._size
+
+    # def set_size(self, size):
+    #      if isinstance(size, int):
+    #         self._size = size
+    #      else:
+    #         raise ValueError("Size must be an integer.")
+
+    # def can_cobble(self):
+    #     print("The shoe has been repaired.")
